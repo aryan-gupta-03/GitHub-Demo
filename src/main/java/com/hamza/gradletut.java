40aa071bc651869eec1587433f5952b71f254605
aryan gupta <aryangupta@gmail.com>
2024-04-03 11:21:45 +0530

    Update

--- src/main/java/com/hamza/gradletut.java
+++ src/main/java/com/hamza/gradletut.java
@@ -2,6 +2,6 @@ package com.hamza ;
 
 public class gradletut{
     public static void main(String []args){
-        System.out.println("This is a test Java application");
+        System.out.println("Java is GOAT");
     }
 }
